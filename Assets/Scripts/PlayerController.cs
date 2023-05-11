d880f5edda7db968016a218f8e5a8fbd527f7ebc
KradGm <enzo.loliveira@hotmail.com>
2023-05-11 20:10:42 -0300

    Movement
    move implement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] public GameObject player;
+    [SerializeField] public Transform playerTransform;
 
-    // Update is called once per frame
+    public float moveSpeed = 0f;
+    [SerializeField] private float jumpForce = 0f;
+    [SerializeField] private float healthLife = 0f;
+
+    public CharacterController characterController;
+
+    public void Start()
+    {
+
+    }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("A");
+            // characterController.SimpleMove(novaPosition * moveSpeed * Time.deltaTime);
+           playerTransform.position = new Vector3(playerTransform.position.x + moveSpeed * Time.deltaTime, 0f, playerTransform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("A");
+            // characterController.SimpleMove(novaPosition * moveSpeed * Time.deltaTime);
+            playerTransform.position = new Vector3(playerTransform.position.x - moveSpeed * Time.deltaTime, 0f, playerTransform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("A");
+            // characterController.SimpleMove(novaPosition * moveSpeed * Time.deltaTime);
+            playerTransform.position = new Vector3(playerTransform.position.x,0f, playerTransform.position.z + moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("A");
+            // characterController.SimpleMove(novaPosition * moveSpeed * Time.deltaTime);
+            playerTransform.position = new Vector3(playerTransform.position.x, 0f, playerTransform.position.z - moveSpeed * Time.deltaTime);
+            
+        }
+
     }
 }
