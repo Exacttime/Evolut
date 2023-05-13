eada17a5569855463d8f72bd8c933f7a4093713b
KradGm <enzo.loliveira@hotmail.com>
2023-05-12 21:35:10 -0300

    Enemy first AI

---
+++ Assets/Scripts/EnemyIA.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIA : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float rangeToChasePlayer = 10f;
+
+    [SerializeField] Rigidbody enemyRigidbody;
+    [SerializeField] Vector3 direction;
+    private bool chasingPlayer = false;
+    [SerializeField] GameObject player;
+
+    void Start()
+    { 
+        direction = RandomDirection();
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+            if (distanceToPlayer <= rangeToChasePlayer)
+            {
+                direction = (player.transform.position - transform.position).normalized;
+                chasingPlayer = true;
+            }
+            else
+            {
+                if (chasingPlayer)
+                {
+                    direction = RandomDirection();
+                    chasingPlayer = false;
+                }
+            }
+
+            enemyRigidbody.MovePosition(transform.localPosition + direction * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private Vector3 RandomDirection()
+    {
+        return new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f)).normalized;
+    }
+}
--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -3,17 +3,5 @@
 public class Jump : MonoBehaviour
 {    
     private PlayerController playerController;
-    public bool IsGrounded()
-    {   
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position,Vector3.down, out hit, playerController.ray, playerController.floorLayer))
-        {
-
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+   
 }
