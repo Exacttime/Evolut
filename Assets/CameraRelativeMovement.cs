8316ab173f655f88a51eeaa3e7a4f3ce8be7c40d
Exacttime <112515202+Exacttime@users.noreply.github.com>
2023-05-17 20:06:12 -0300

    New Movement System

---
+++ Assets/CameraRelativeMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraRelativeMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController characterController;
+    Vector3 playerInput;
+    float horizontalInput;
+    Vector3 cameraRelativeMovement;
+    float verticalInput;
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        playerInput.x = horizontalInput;
+        playerInput.z = verticalInput;
+        cameraRelativeMovement = ConvertToCameraSpace(playerInput);
+        characterController.Move(cameraRelativeMovement * Time.deltaTime);
+    }
+    Vector3 ConvertToCameraSpace(Vector3 vectorRotate)
+    {
+        float currentYValue = vectorRotate.y;
+        Vector3 cameraForward = Camera.main.transform.forward;
+        Vector3 cameraRight = Camera.main.transform.right;
+
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        cameraForward = cameraForward.normalized;
+        cameraRight = cameraRight.normalized;
+
+        Vector3 cameraForwardProductZ = vectorRotate.z * cameraForward;
+        Vector3 cameraRightProductx = vectorRotate.x * cameraRight;
+
+        Vector3 rotateResultWorldSpace = cameraForwardProductZ + cameraRightProductx;
+        rotateResultWorldSpace.y = currentYValue;
+        return rotateResultWorldSpace;
+    }
+    void HandleRotation()
+    {
+        Vector3 positionToLookAt;
+        positionToLookAt.x = cameraRelativeMovement.x;
+        positionToLookAt.y = 0;
+        positionToLookAt.z = cameraRelativeMovement.z;
+    }
+}
